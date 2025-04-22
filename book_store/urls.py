31c2909894f83d560aaeb4ded74de61bc0f42482
Munoto <zeron.masimov@mail.ru>
2025-04-22 13:43:16 +0500

    added cart and CRUD operations

--- book_store/urls.py
+++ book_store/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.urls import path
 
 from book_store import settings
-from store.views import logout_view, home_view, register_view, login_view, create_author, create_book, book_detail
+from store.views import logout_view, home_view, register_view, login_view, create_author, create_book, book_detail, \
+    edit_book, delete_book, add_to_cart, cart_view, clear_cart, remove_from_cart
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,12 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('create_book/', create_book, name='create_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
     path('create_author/', create_author, name='create_author'),
     path('book/<int:pk>/', book_detail, name='book_detail'),
+    path('cart/add/<int:book_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart_view, name='cart'),
+    path('cart/clear/', clear_cart, name='clear_cart'),
+    path('cart/remove/<int:book_id>/', remove_from_cart, name='remove_from_cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,8 +1,9 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
 from store.forms import CustomUserCreationForm, CustomLoginForm, BookForm, AuthorForm
 from store.models import Book
@@ -75,3 +76,74 @@ def create_author(request):
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'book_detail.html', {'book': book})
+
+
+@staff_member_required
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form, 'book': book})
+
+
+@staff_member_required
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('home')
+    return render(request, 'confirm_delete.html', {'book': book})
+
+
+@login_required(login_url='login')
+def add_to_cart(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    cart = request.session.get('cart', {})
+
+    if str(book_id) in cart:
+        cart[str(book_id)] += 1
+    else:
+        cart[str(book_id)] = 1
+
+    request.session['cart'] = cart
+
+    return redirect('cart')
+
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+    books = []
+    total_price = 0
+
+    for book_id, quantity in cart.items():
+        book = Book.objects.get(id=book_id)
+        book.quantity = quantity
+        book.total = book.price * quantity
+        books.append(book)
+        total_price += book.total
+
+    context = {
+        'books': books,
+        'total_price': total_price,
+    }
+    return render(request, 'cart.html', context)
+
+
+@require_POST
+def clear_cart(request):
+    request.session['cart'] = {}
+    return redirect('cart')
+
+
+def remove_from_cart(request, book_id):
+    cart = request.session.get('cart', {})
+    if book_id in cart:
+        del cart[book_id]
+        request.session['cart'] = cart
+    return redirect('cart')
